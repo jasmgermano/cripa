7ecb81aad5a9b00351815db18b3b19e58b6beb48
jasmine <jasmigermano@gmail.com>
2024-10-21 19:04:29 -0300

    tirando console.log

--- src/hooks/useCripa.tsx
+++ src/hooks/useCripa.tsx
@@ -63,8 +63,6 @@ export default function useCripa() {
             }
         }
 
-        console.log("Soluções", solutions);
-
         setSolutions(solutions);
         setTerm(normalizedTerm);
     };
